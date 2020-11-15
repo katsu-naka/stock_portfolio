0e34e2fb68610af0a528ba773fbfaccb0f5d7d11
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-15 20:23:48 +0900

    詳細ページのビュー実装

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def previous
+    Product.where("id < ?",self.id).order("id DESC").first
+  end
+
+  def next
+    Product.where("id > ?", self.id).order("id ASC").first
+  end
 end
