e03dfbb96c4ce208aeb2ab291398e87b01b94fa6
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-15 15:45:59 +0900

    deviseビューファイルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
-
-NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-
-with_options presence: true do
+         
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  
+  with_options presence: true do
   validates :nickname
   validates :birth
   validates :first_name, format: { with: NAME_REGEX, message: 'は全角で入力してください' }
   validates :last_name, format: { with: NAME_REGEX, message: 'は全角で入力してください' }
   validates :first_name_kana, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
   validates :last_name_kana, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }
+  end
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, format: { with: PASSWORD_REGEX, message: 'は半角英数字両方を含めて入力してください' }
+      
 end
-validates :email, uniqueness: { case_sensitive: false }
-validates :password, format: { with: PASSWORD_REGEX, message: 'は半角英数字両方を含めて入力してください' }
