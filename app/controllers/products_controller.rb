8f7c6aeb10873ac968ddcf3705052d62f5b44129
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-15 17:54:08 +0900

    投稿保存機能の実装
    productsコントローラー編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,21 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @prodact = Prodact.new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title,:text,:image,:github_uri,:product_uri).merge(user_id: current_user.id)
   end
 end
