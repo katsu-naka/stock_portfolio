42d8a6afa4b7ff40036a5df37c106962e43c66f6
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-17 18:29:40 +0900

    ライク機能（非同期）実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,17 @@
 class LikesController < ApplicationController
+  before_action :product_params
+
   def create
     Like.create(user_id: current_user.id, product_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, product_id: params[:id]).destroy
-    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    @product = Product.find(params[:id])
   end
 end
