b1078795288f2d4cf8ad2de8d1b4f685823c84fa
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-20 22:35:57 +0900

    capistrano導入

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+lock '3.14.1'
+
+# Capistranoのログの表示に利用する
+set :application, 'stock_portfolio'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:katsu-naka/stock_portfolio.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5' 
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/stockportfolio30436.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
